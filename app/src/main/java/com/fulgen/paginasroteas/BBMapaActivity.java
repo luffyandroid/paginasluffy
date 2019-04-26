c3d10f185e4978b6149abe7b2425d60b6404cc78
fullgen <fulgenll@hotmail.com>
2019-04-26 09:24:38 +0200

    -Cosas del mapa

--- app/src/main/java/com/fulgen/paginasroteas/BBMapaActivity.java
+++ app/src/main/java/com/fulgen/paginasroteas/BBMapaActivity.java
@@ -1,14 +1,24 @@
 package com.fulgen.paginasroteas;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.VectorDrawable;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,6 +28,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.getbase.floatingactionbutton.FloatingActionsMenu;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -28,6 +39,8 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Locale;
 
 public class BBMapaActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -132,6 +145,7 @@ public class BBMapaActivity extends AppCompatActivity implements OnMapReadyCallb
 
     public void clickLocalizarmeBB(View v) {
 
+        locationStart();
         //PARA QUE SE CIERRE AL PULSAR
         menu_fabBB.collapse();
     }
@@ -163,12 +177,17 @@ public class BBMapaActivity extends AppCompatActivity implements OnMapReadyCallb
         finish();
     }
 
-
+    //PROTOCOLO GOOGLE MAPS
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
         mMap = googleMap;
 
+        CameraUpdate inicioSatelite =
+                CameraUpdateFactory.newLatLngZoom(new LatLng(36.6418169,-6.1849189),10);
+        mMap.moveCamera(inicioSatelite);
+
+        //ESTILO DEL MAPA
         try {
             // Customise the styling of the base map using a JSON object defined
             // in a raw resource file.
@@ -184,16 +203,17 @@ public class BBMapaActivity extends AppCompatActivity implements OnMapReadyCallb
         }
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(36.6269284, -6.3531886);
         mMap.addMarker(new MarkerOptions()
                 .position(sydney)
                 //.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_marcador_001))
                 .icon(getBitmapDescriptor(R.drawable.ic_marcador_a_carniceria))
-                .title("Marker in Sydney"));
+                .title("Prueba"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
     }
 
+    //PROTOCOLO PARA CONVERTIR SVG A MAPA DE BIT
     private BitmapDescriptor getBitmapDescriptor(int id) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             VectorDrawable vectorDrawable = (VectorDrawable) getDrawable(id);
@@ -211,6 +231,113 @@ public class BBMapaActivity extends AppCompatActivity implements OnMapReadyCallb
 
         } else {
             return BitmapDescriptorFactory.fromResource(id);
+        }
+    }
+
+    //PROTOCOLO LOCALIZACIÓN GPS
+    private void locationStart() {
+        LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Localizacion Local = new Localizacion();
+        Local.setMainActivity(this);
+        final boolean gpsEnabled = mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!gpsEnabled) {
+            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(settingsIntent);
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+            return;
+        }
+        mlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, (LocationListener) Local);
+        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) Local);
+
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1000) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationStart();
+                return;
+            }
+        }
+    }
+
+    public void setLocation(Location loc) {
+        //Obtener la direccion de la calle a partir de la latitud y la longitud
+        if (loc.getLatitude() != 0.0 && loc.getLongitude() != 0.0) {
+            try {
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                List<Address> list = geocoder.getFromLocation(
+                        loc.getLatitude(), loc.getLongitude(), 1);
+
+                if (!list.isEmpty()) {
+                        CameraUpdate locactual =
+                                CameraUpdateFactory.newLatLngZoom(new LatLng(loc.getLatitude(), loc.getLongitude()), 17);
+                        mMap.animateCamera(locactual);
+
+
+
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /* Aqui empieza la Clase Localizacion */
+    public class Localizacion implements LocationListener {
+        BBMapaActivity baMapaFinalActivity;
+
+        public BBMapaActivity getBaMapaFinalActivity() {
+            return baMapaFinalActivity;
+        }
+
+        public void setMainActivity(BBMapaActivity baMapaFinalActivity) {
+            this.baMapaFinalActivity = baMapaFinalActivity;
+        }
+
+        @Override
+        public void onLocationChanged(Location loc) {
+            // Este metodo se ejecuta cada vez que el GPS recibe nuevas coordenadas
+            // debido a la deteccion de un cambio de ubicacion
+
+            loc.getLatitude();
+            loc.getLongitude();
+
+
+            this.baMapaFinalActivity.setLocation(loc);
+
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            // Este metodo se ejecuta cuando el GPS es desactivado
+            //mensaje1.setText("GPS Desactivado");
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // Este metodo se ejecuta cuando el GPS es activado
+            //mensaje1.setText("GPS Activado");
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            switch (status) {
+                case LocationProvider.AVAILABLE:
+                    Log.d("debug", "LocationProvider.AVAILABLE");
+                    break;
+                case LocationProvider.OUT_OF_SERVICE:
+                    Log.d("debug", "LocationProvider.OUT_OF_SERVICE");
+                    break;
+                case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                    Log.d("debug", "LocationProvider.TEMPORARILY_UNAVAILABLE");
+                    break;
+            }
         }
     }
 
